90a28986ee1844ca7f0616b6a4a58106541d8d94
Joshua Helt <jahelt422@gmail.com>
2026-09-23 18:56:21 -0500

    Add Trip and Stop models with initial migration

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+    id = models.IntegerField(primary_key=True)
+    trip_name = models.CharField(max_length=32)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    time_created = models.DateField(auto_now_add=True)
+
+    def __repr__(self):
+        return f'<Trip #{self.id}: {self.trip_name}>'
+
+
+class Stop(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    id = models.IntegerField(primary_key=True)
+    city_name = models.CharField(max_length=32)
+    arrival_date = models.DateField()
+    departure_date = models.DateField()
+
+    def __repr__(self):
+        return f'<Stop #{self.id}: {self.city_name}>'
